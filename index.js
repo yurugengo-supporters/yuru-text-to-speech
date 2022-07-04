fdffd4d547ab124c40d00c42cbc4d6fc1f2cb412
giza10-michiko <97360836+giza10-michiko@users.noreply.github.com>
2022-07-04 22:49:13 +0900

    話者選択モーダル

--- index.js
+++ index.js
@@ -2,67 +2,158 @@ require('dotenv').config();
 const aws = require('aws-sdk');
 const fs = require('fs');
 const tmp = require("tmp");
-const { Client, Intents, MessageEmbed  } = require('discord.js');
-
+const { Client, Intents, MessageEmbed   } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const discordModals = require('discord-modals');
+const { Modal, TextInputComponent, SelectMenuComponent,showModal  } = discordModals; 
 
-client.once('ready', () => {
+discordModals(client);
+
+const _embed_color='#d000e0';
+const _modal_id='modal-customid';
+let _mymodal=null;
+
+const _polly_describeVoices = async () =>{
+	let polly = new aws.Polly({ apiVersion: '2016-06-10', region: 'us-east-1'});
+	return await polly.describeVoices({}).promise().then(x=>{return x;});
+};
+
+const _polly_speech = async(washa, ipa) =>{
+	let polly = new aws.Polly({ apiVersion: '2016-06-10', region: 'us-east-1'});
+	let speechParams = {
+		//Engine : voice_engine["engine"],// "neural",//"standard",
+		OutputFormat: 'mp3',
+		VoiceId:washa,// "Joanna",
+		Text: "<phoneme alphabet='ipa' ph='" + ipa + "'></phoneme>",
+		SampleRate: '22050',
+		TextType: 'ssml'
+	};
+	return await polly.synthesizeSpeech(speechParams).promise().then(z=>{ return z;});
+};
+
+const _create_options = (voices)=>{
+	let arr= voices.map(_ => {
+		return {
+			label: _["LanguageName"],
+			value: _["Id"],
+		};
+	});
+	return arr;
+};
+client.once('ready', async () => {
+	console.log('describeVoices');
+	const data = await _polly_describeVoices();
+	const codes = Array.from(new Set( data["Voices"].map(_ => _["LanguageCode"])));
+	const voices = codes.map(code=> data["Voices"].find(_ => _["LanguageCode"] == code));
+	const options_en = _create_options(voices.filter(_=>_["LanguageCode"].startsWith("en-")));
+	const options_other = _create_options(voices.filter(_=>!_["LanguageCode"].startsWith("en-")));	
+	options_en.unshift({
+		label:"なし（↓で選択）",
+		value:"*",
+		default:true,
+	});
+	options_other.unshift({
+		label:"なし（↑で選択）",
+		value:"*",
+		default:true,
+	});
+	const txtIpa=new TextInputComponent() 
+		.setCustomId('ipa')
+		.setLabel('IPA文字列')
+		.setStyle('SHORT') 
+		.setRequired(true);
+	const cmbWasha_en=new SelectMenuComponent() 
+		.setCustomId('washa_en')
+		.addOptions(options_en.slice(0,25));
+	const cmbWasha_other=new SelectMenuComponent() 
+		.setCustomId('washa_other')
+		.addOptions(options_other.slice(0,25));
+	const txtMemo=new TextInputComponent() 
+		.setCustomId('memo')
+		.setLabel('説明や要約')
+		.setStyle('SHORT')
+		.setRequired(false);
+	_mymodal = new Modal()
+		.setCustomId(_modal_id)
+		.setTitle('Yuru-Text-To-Speech')
+		.addComponents(txtIpa, cmbWasha_en, cmbWasha_other, txtMemo);
     console.log('ready');
 });
 
-const _command_ipa = async (interaction) => {
-    const ipa = interaction.options.getString("ipa_text");
-    const label = interaction.options.getString("label_text", false);
-    await interaction.deferReply();
-    let polly = new aws.Polly({ apiVersion: '2016-06-10', region: 'us-east-1'});
-    let speechParams = {
-        OutputFormat: 'mp3',
-        VoiceId: "Joanna",
-        Text: "<phoneme alphabet='ipa' ph='" + ipa + "'></phoneme>",
-        SampleRate: '22050',
-        TextType: 'ssml'
-    };
-    await polly.synthesizeSpeech(speechParams).promise().then(data => {
-        tmp.file( {  postfix: '.mp3' },async (err, name, fd, cleanupCallback) => {
-            if (err) {
-                console.log(err);
-                await interaction.editReply("tmpファイル作成に失敗しました。BOT管理者に連絡してください");
-                return;
-            }
-            fs.writeFile(fd, data.AudioStream,async  (err) => {
-                if (err) {
-                    console.log(err);
-                    await interaction.editReply("mp3ファイル書き込みに失敗しました。BOT管理者に連絡してください");
-                    return;
-                }
-                const embed = new MessageEmbed()
-                    .setColor('#d000e0')
-                    .setTitle(label == null ? ipa : label);
-                await interaction.editReply(
-                    {
-                        embeds: [embed],
-                        files: [{ "attachment": name }]
-                    }
-                );
-            });
-        });
-    }).catch(async err => {
-        console.log(err);
-        const embed = new MessageEmbed()
-            .setColor('#d000e0')
-            .setTitle("音声変換に失敗しました。正しいIPA表記で入力してください");
-        await interaction.editReply({ embeds: [embed]});
-	});
-};
-
 client.on('interactionCreate', async interaction => { 
-    if (!interaction.isCommand()) return;
+    if (interaction.isCommand()) {
+		const { commandName } = interaction;
 
-    const { commandName } = interaction;
+		if (commandName == "ipa") {
+			await showModal(_mymodal, {client:client, interaction:interaction});
+		}
+	}
+});
 
-    if (commandName === 'ipa') {
-        await _command_ipa(interaction);
-    }
+client.on("modalSubmit", async(modal)=>{
+	if(modal.customId === _modal_id) {
+		await modal.deferReply();
+		const ipa = modal.getTextInputValue('ipa');
+		const washa_en = modal.getSelectMenuValues('washa_en')[0];
+		const washa_other = modal.getSelectMenuValues('washa_other')[0];
+		const memo = modal.getTextInputValue("memo");
+		if (washa_en == "*" && washa_other == "*") {
+			await modal.followUp(
+				{
+					embeds: [
+						new MessageEmbed()
+						.setColor(_embed_color)
+						.setTitle("話者が選択されていません。")
+					],
+				}
+			);
+			return;
+		}
+		let washa = washa_en;
+		if (washa_other != "*") {
+			washa = washa_other;
+		}
+		console.log(washa);
+		let stream;
+		try {
+			const data = await _polly_speech(washa, ipa);
+			stream = data.AudioStream;
+		} catch (ex) {
+			await modal.followUp(
+				{
+					embeds: [
+						new MessageEmbed()
+						.setColor(_embed_color)
+						.setTitle("音声変換に失敗しました。")
+					],
+				}
+			);
+			return;
+		}
+		const tmpobj = tmp.fileSync({  postfix: '.mp3' });
+		if (tmpobj.err) {
+			console.log(tmpobj.err);
+			await modal.followUp("tmpファイル作成に失敗しました。BOT管理者に連絡してください");
+			return;
+		}
+		try {
+			fs.writeFileSync(tmpobj.fd, stream);
+			await modal.followUp(
+				{
+					embeds: [
+						new MessageEmbed()
+						.setColor(_embed_color)
+						.setTitle(memo == null ? ipa : memo)
+					],
+					files: [{ "attachment": tmpobj.name }]
+				}
+			);	
+		} catch (ex) {
+			console.log(ex);
+			await modal.followUp("mp3ファイル書き込みに失敗しました。BOT管理者に連絡してください");
+			return;
+		}
+	}
 });
 
 client.login(process.env.DISCORD_TOKEN);
--- reg.js
+++ reg.js
@@ -1,25 +1,9 @@
 require('dotenv').config();
 const { Client, Intents, ClientApplication } = require('discord.js');
-const commands = [
-    {
-        name: "ipa",
-        description: "IPA文字列を音声に変換",
-        options: [
-            {
-                type: "STRING",
-                name: "ipa_text",
-				nameLocalizations :[],
-                description: "IPA文字列",
-                required: true
-            },
-            {
-                type: "STRING",
-                name: "label_text",
-                description: "IPA文字列の要約",
-                required: false 
-            },
-        ]
-    }
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const commands=[
+	new SlashCommandBuilder().setName("ipa").setDescription("IPAを音声に変換")
 ];
 const guildId = process.argv[2];
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -27,9 +11,6 @@ client.token = process.env.DISCORD_TOKEN;
 
 (async () => {
     client.application = new ClientApplication(client, {});
-    await client.application.fetch();
-    await client.application.commands.set([], guildId);
-	
     await client.application.fetch();
     await client.application.commands.set(commands, guildId);
     console.log("success!");
