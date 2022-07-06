1788b5adc66c949bb6a80fe2f7045b36c851c88c
giza10-michiko <97360836+giza10-michiko@users.noreply.github.com>
2022-07-06 22:35:21 +0900

    言語を３つのグループに分けてサブコマンドに

--- index.js
+++ index.js
@@ -4,14 +4,8 @@ const fs = require('fs');
 const tmp = require("tmp");
 const { Client, Intents, MessageEmbed   } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const discordModals = require('discord-modals');
-const { Modal, TextInputComponent, SelectMenuComponent,showModal  } = discordModals; 
-
-discordModals(client);
 
 const _embed_color='#d000e0';
-const _modal_id='modal-customid';
-let _mymodal=null;
 let _voices = [];
 
 const _polly_describeVoices = async () =>{
@@ -32,58 +26,19 @@ const _polly_speech = async(washa, engine, ipa) =>{
 	return await polly.synthesizeSpeech(speechParams).promise().then(z=>{ return z;});
 };
 
-const _create_options = (voices)=>{
-	let arr= voices.map(_ => {
-		return {
-			label: _["LanguageName"],
-			value: _["Id"],
-			default:false,
-		};
-	});
-	return arr;
+const _select_voice = (code) => {
+	const list = _voices.filter(_=> _["LanguageCode"] == code);
+	let voice = list.find(_=> _["SupportedEngines"].findIndex(a => a=="standard")!=-1);
+	if (voice != undefined) {
+		return voice;
+	}
+	return list[0];
 };
+
 client.once('ready', async () => {
 	console.log('describeVoices');
 	const data = await _polly_describeVoices();
 	_voices = data["Voices"];
-	const codes = Array.from(new Set( _voices.map(_ => _["LanguageCode"])));
-	const voices = codes.map(code=> _voices.find(_ => _["LanguageCode"] == code));
-	const options_en = _create_options(voices.filter(_=>_["LanguageCode"].startsWith("en-")));
-	const options_other = _create_options(voices.filter(_=>!_["LanguageCode"].startsWith("en-")));	
-	options_en.unshift({
-		label:"英語はここで選択",
-		value:"*",
-		default:false,
-	});
-	options_other.unshift({
-		label:"英語以外はここで選択",
-		value:"*",
-		default:false,
-	});
-	const en_us_name = _voices.find(_ => _["LanguageCode"] == "en-US")["LanguageName"];
-	console.log(en_us_name);
-	options_en.find(_ => _["label"] == en_us_name)["default"] = true;
-	options_other[0]["default"]=true;
-	const txtIpa=new TextInputComponent() 
-		.setCustomId('ipa')
-		.setLabel('IPA文字列')
-		.setStyle('SHORT') 
-		.setRequired(true);
-	const cmbWasha_en=new SelectMenuComponent() 
-		.setCustomId('washa_en')
-		.addOptions(options_en.slice(0,25));
-	const cmbWasha_other=new SelectMenuComponent() 
-		.setCustomId('washa_other')
-		.addOptions(options_other.slice(0,25));
-	const txtMemo=new TextInputComponent() 
-		.setCustomId('memo')
-		.setLabel('説明や要約')
-		.setStyle('SHORT')
-		.setRequired(false);
-	_mymodal = new Modal()
-		.setCustomId(_modal_id)
-		.setTitle('Yuru-Text-To-Speech')
-		.addComponents(txtIpa, cmbWasha_en, cmbWasha_other, txtMemo);
     console.log('ready');
 });
 
@@ -92,88 +47,53 @@ client.on('interactionCreate', async interaction => {
 		const { commandName } = interaction;
 
 		if (commandName == "ipa") {
-			await showModal(_mymodal, {client:client, interaction:interaction});
-		}
-	}
-});
-
-client.on("modalSubmit", async(modal)=>{
-	if(modal.customId === _modal_id) {
-		await modal.deferReply();
-		const ipa = modal.getTextInputValue('ipa');
-		const washa_en = modal.getSelectMenuValues('washa_en')[0];
-		const washa_other = modal.getSelectMenuValues('washa_other')[0];
-		const memo = modal.getTextInputValue("memo");
-		if (washa_en == "*" && washa_other == "*") {
-			await modal.followUp(
-				{
-					embeds: [
-						new MessageEmbed()
-						.setColor(_embed_color)
-						.setTitle("話者が選択されていません。")
-					],
-				}
-			);
-			return;
-		}
-		let washa = washa_en;
-		if (washa_other != "*") {
-			washa = washa_other;
-		}
-		const voice = _voices.find(_=> _["Id"] == washa);
-		if (voice == undefined) {
-			await modal.followUp(
-				{
-					embeds: [
-						new MessageEmbed()
-						.setColor(_embed_color)
-						.setTitle("無効な話者です。")
-					],
-				}
-			);
-			return;
-		}
-		console.log(voice);
-		const engine = voice["SupportedEngines"][0];
-		let stream;
-		try {
-			const data = await _polly_speech(washa, engine, ipa);
-			stream = data.AudioStream;
-		} catch (ex) {
-			console.log(ex);
-			await modal.followUp(
-				{
-					embeds: [
-						new MessageEmbed()
-						.setColor(_embed_color)
-						.setTitle("音声変換に失敗しました。")
-					],
-				}
-			);
-			return;
-		}
-		const tmpobj = tmp.fileSync({  postfix: '.mp3' });
-		if (tmpobj.err) {
-			console.log(tmpobj.err);
-			await modal.followUp("tmpファイル作成に失敗しました。BOT管理者に連絡してください");
-			return;
-		}
-		try {
-			fs.writeFileSync(tmpobj.fd, stream);
-			await modal.followUp(
-				{
-					embeds: [
-						new MessageEmbed()
-						.setColor(_embed_color)
-						.setTitle(memo == null ? ipa : memo)
-					],
-					files: [{ "attachment": tmpobj.name }]
-				}
-			);	
-		} catch (ex) {
-			console.log(ex);
-			await modal.followUp("mp3ファイル書き込みに失敗しました。BOT管理者に連絡してください");
-			return;
+			const ipa = interaction.options.getString("ipa");
+			const code = interaction.options.getString("speaker")
+			const label = interaction.options.getString("label", false);
+			await interaction.deferReply();
+			const voice = _select_voice(code);
+			const engines = voice["SupportedEngines"];
+			console.log(voice);
+			let stream;
+			try {
+				const data = await _polly_speech(voice["Id"], engines[engines.length-1], ipa);
+				stream = data.AudioStream;
+			} catch (ex) {
+				console.log(ex);
+				await interaction.followUp(
+					{
+						embeds: [
+							new MessageEmbed()
+							.setColor(_embed_color)
+							.setTitle("音声変換に失敗しました。")
+						],
+					}
+				);
+				return;
+			}
+			const tmpobj = tmp.fileSync({ postfix: '.mp3' });
+			if (tmpobj.err) {
+				console.log(tmpobj.err);
+				await interaction.followUp("tmpファイル作成に失敗しました。BOT管理者に連絡してください");
+				return;
+			}
+			try {
+				fs.writeFileSync(tmpobj.fd, stream);
+				await interaction.followUp(
+					{
+						embeds: [
+							new MessageEmbed()
+							.setColor(_embed_color)
+							.setTitle(label == null ? ipa : label)
+						],
+						files: [{ "attachment": tmpobj.name }]
+					}
+				);	
+			} catch (ex) {
+				console.log(ex);
+				await interaction.followUp("mp3ファイル書き込みに失敗しました。BOT管理者に連絡してください");
+				return;
+			}
 		}
 	}
 });
--- reg.js
+++ reg.js
@@ -1,9 +1,131 @@
 require('dotenv').config();
 const { Client, Intents, ClientApplication } = require('discord.js');
-const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const cmd_ipa_eng={
+	type: "SUB_COMMAND",
+	name: "eng",
+	description: "英語圏",
+	options: [
+		{
+			type:"STRING",
+			name:"ipa",
+			description:"IPA文字列",
+			required:true,
+		},
+		{
+			type: "STRING",
+			name: "speaker",
+			description: "英語圏の話者を選択",
+			required: true,
+			choices: [
+				{ name: "Australian English", value: "en-AU" },
+				{ name: "British English", value: "en-GB" },
+				{ name: "Indian English", value: "en-IN" },
+				{ name: "New Zealand Englis", value: "en-NZ" },
+				{ name: "South African English", value: "en-ZA" },
+				{ name: "US English", value: "en-US" },
+				{ name: "Welsh English", value: "en-GB-WLS" },
+			]
+		},
+		{
+			type:"STRING",
+			name:"label",
+			description:"要約や説明",
+			required:false,
+		},
+	],
+};
+const cmd_ipa_asia={
+	type: "SUB_COMMAND",
+	name: "asia",
+	description: "アジア圏",
+	options: [
+		{
+			type:"STRING",
+			name:"ipa",
+			description:"IPA文字列",
+			required:true,
+		},
+		{
+			type: "STRING",
+			name: "speaker",
+			description: "アジア圏の話者を選択",
+			required: true,
+			choices: [
+				{ name: "Chinese Mandarin", value: "cmn-CN" },
+				{ name: "Japanese", value: "ja-JP" },
+				{ name: "Korean", value: "ko-KR" },
+			]
+		},
+		{
+			type:"STRING",
+			name:"label",
+			description:"要約や説明",
+			required:false,
+		}
+	],
+};
+const cmd_ipa_others={
+	type: "SUB_COMMAND",
+	name: "others",
+	description: "その他",
+	options: [
+		{
+			type:"STRING",
+			name:"ipa",
+			description:"IPA文字列",
+			required:true,
+		},
+		{
+			type: "STRING",
+			name: "speaker",
+			description: "その他の話者を選択",
+			required: true,
+			choices: [
+				{ name: "Arabic", value: "arb" },
+				{ name: "Austrian Germa", value: "de-AT" },
+				{ name: "Brazilian Portuguese", value: "pt-BR" },
+				{ name: "Canadian French", value: "fr-CA" },
+				{ name: "Castilian Spanish", value: "es-ES" },
+				{ name: "Catalan", value: "ca-ES" },
+				{ name: "Danish", value: "da-DK" },
+				{ name: "Dutch", value: "nl-NL" },
+				{ name: "French", value: "fr-FR" },
+				{ name: "German", value: "de-DE" },
+				{ name: "Icelandic", value: "is-IS" },
+				{ name: "Italian", value: "it-IT" },
+				{ name: "Mexican Spanish", value: "es-MX" },
+				{ name: "Norwegian", value: "nb-NO" },
+				{ name: "Polish", value: "pl-PL" },
+				{ name: "Portuguese", value: "pt-PT" },
+				{ name: "Romanian", value: "ro-RO" },
+				{ name: "Russian", value: "ru-RU" },
+				{ name: "Swedish", value: "sv-SE" },
+				{ name: "Turkish", value: "tr-TR" },
+				{ name: "US Spanish", value: "es-US" },
+				{ name: "Welsh", value: "cy-GB" },
+			]
+		},
+		{
+			type:"STRING",
+			name:"label",
+			description:"要約や説明",
+			required:false,
+		}
+	],
+};
+const cmd_ipa = {
+	name: "ipa",
+	description: "IPAを音声に変換",
+	options: [
+		cmd_ipa_eng,
+		cmd_ipa_asia,
+		cmd_ipa_others
+	],
+};
 
 const commands=[
-	new SlashCommandBuilder().setName("ipa").setDescription("IPAを音声に変換")
+	cmd_ipa
 ];
 const guildId = process.argv[2];
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
